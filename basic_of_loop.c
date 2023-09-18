10d08726061674bdb7e90236cca56d1dab2e7f38
monarch-009 <adityasinghmoni@gmail.com>
2023-09-18 12:23:08 +0530

    for loop

---
+++ basic_of_loop.c
@@ -0,0 +1,13 @@
+// Print Hello world 'n' times Take n as a input from the user;
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the N value:-\n");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        printf("Aditya Raj\n");
+    }
+    return 0;
+}
