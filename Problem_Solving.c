15c2c94b60905d1eed085c6a32b782ca1d67dbc7
monarch-009 <adityasinghmoni@gmail.com>
2023-09-18 11:56:37 +0530

    Problem Solving-2

---
+++ Problem_Solving.c
@@ -0,0 +1,17 @@
+//Given the length and breadth of rectangle ,write a program to find whether the area of the rectangle is greater than its perimeter;
+#include<stdio.h>
+int main()
+{
+    float l,b,a,p;
+    printf("Enter the Length:-\n");
+    scanf("%f",&l);
+    printf("Enter the Breadth:-\n");
+    scanf("%f",&b);
+    a = l*b;
+    p = 2*(l+b);
+    if (a>p)
+    {
+        printf("Area is greater than Perimeter\n");
+    }
+
+}
