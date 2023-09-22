67b73c57177a5eca26ddf55731b61d2c10fa63f8
monarch-009 <adityasinghmoni@gmail.com>
2023-09-22 12:54:15 +0530

    pattern printing

---
+++ alphabetsquare.c
@@ -0,0 +1,27 @@
+/*
+Print the given pattern
+A B C D
+A B C D
+A B C D
+A B C D
+*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a Number:-");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        int a = 1;
+        for (int j = 1; j <= n; j++)
+        {
+            int d = a + 64;
+            char ch = (char)d;
+            printf("%c ", ch);
+            a++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
