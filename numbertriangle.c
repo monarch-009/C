6c64f59ba3588d584fda2852a8ee8211c03f4150
monarch-009 <adityasinghmoni@gmail.com>
2023-09-24 21:17:59 +0530

    pattern printing

---
+++ numbertriangle.c
@@ -0,0 +1,29 @@
+/*
+Enter a number:-
+9
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+1 2 3 4 5 6 
+1 2 3 4 5 6 7 
+1 2 3 4 5 6 7 8 
+1 2 3 4 5 6 7 8 9 
+*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number:-\n");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
