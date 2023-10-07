97955e4c4177371ef46e25285c45bb122a6eafd5
monarch-009 <adityasinghmoni@gmail.com>
2023-10-07 22:36:47 +0530

    function basic

---
+++ Function_Basic.c
@@ -0,0 +1,14 @@
+// Basic of Function
+#include <stdio.h>
+void greet() // Function
+{
+    printf("Good Morning\n");
+    printf("How Are You\n");
+    return;
+}
+
+int main()
+{
+    greet(); // Call Function
+    return 0;
+}
